00b2654f30e87799eed625bc0680990830f02eaa
Ade8018 <tomatomaster@hotmail.com>
2016-05-04 13:42:37 +0800

    修改参数

--- HelloAd/src/test/ad/Time.java
+++ HelloAd/src/test/ad/Time.java
@@ -9,9 +9,12 @@ public class Time {
 		Calendar c = Calendar.getInstance();
 		int h = c.get(Calendar.HOUR_OF_DAY);
 		int weekday = c.get(Calendar.DAY_OF_WEEK);
-		int i = 1;
+		float i = 1;
 		if (weekday == 6 || weekday == 7) {
 			i = 2;
+		}
+		if (weekday == 8) {
+			i = 1.5f;
 		}
 		if (h >= 0 && h <= 1) {
 			return Utils.sRandom.nextFloat() < (0.25f * i);
