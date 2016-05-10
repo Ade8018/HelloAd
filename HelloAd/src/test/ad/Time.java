1b96b13c73c9f23e820de9927978cfb65dfaa89f
Ade8018 <tomatomaster@hotmail.com>
2016-05-10 17:14:13 +0800

    --

--- HelloAd/src/test/ad/Time.java
+++ HelloAd/src/test/ad/Time.java
@@ -9,9 +9,12 @@ public class Time {
 		Calendar c = Calendar.getInstance();
 		int h = c.get(Calendar.HOUR_OF_DAY);
 		int weekday = c.get(Calendar.DAY_OF_WEEK);
-		float i = 2.5f;
+		float i = 1.5f;
+		if (weekday == 6 || weekday == 7) {
+			i = 2;
+		}
 		if (weekday == 8) {
-			i = 4f;
+			i = 1.8f;
 		}
 		if (h >= 0 && h <= 1) {
 			return Utils.sRandom.nextFloat() < (0.25f * i);
